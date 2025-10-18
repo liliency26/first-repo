7fe46c352e442bc34d2ec4e843c0d5402dc67da7
liliency26 <leenalsweed26@gmail.com>
2025-10-18 04:20:18 +0300

    remove comment sign

--- src/main/java/com/mycompany/lab_01_ex/Lab_01_EX.java
+++ src/main/java/com/mycompany/lab_01_ex/Lab_01_EX.java
@@ -1,14 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 package com.mycompany.lab_01_ex;
 
 import java.util.Scanner;
 
-/**
- *
- * @author leena
- */
 public class Lab_01_EX {
 
     public static void main(String[] args) {
@@ -24,28 +18,28 @@ public class Lab_01_EX {
         System.out.println("The set contains: ");
         myArr.display();
 
-//        System.out.print("Search for key: ");
-//        int key = read.nextInt();
-//        if (myArr.find(key)) {
-//            System.out.println("Found " + key);
-//        } else {
-//            System.out.println("Can't find " + key);
-//        }
-//
-//        System.out.print("Delete key: ");
-//        key = read.nextInt();
-//        if (myArr.delete(key)) {
-//            System.out.println("Deleted " + key);
-//        } else {
-//            System.out.println("Can't find " + key);
-//        }
-//
-//        System.out.println("The set contains: ");
-//        myArr.display();
-//
-//        //   display() overloaded 
-//        System.out.println("Displaying part of the array (index 1 to 3): ");
-//        myArr.display(1, 3);
+        System.out.print("Search for key: ");
+        int key = read.nextInt();
+        if (myArr.find(key)) {
+            System.out.println("Found " + key);
+        } else {
+            System.out.println("Can't find " + key);
+        }
+
+        System.out.print("Delete key: ");
+        key = read.nextInt();
+        if (myArr.delete(key)) {
+            System.out.println("Deleted " + key);
+        } else {
+            System.out.println("Can't find " + key);
+        }
+
+        System.out.println("The set contains: ");
+        myArr.display();
+
+        //   display() overloaded 
+        System.out.println("Displaying part of the array (index 1 to 3): ");
+        myArr.display(1, 3);
 
         //HomeWorke Lab1
         System.out.print("Enter a number to insertSorted: ");
